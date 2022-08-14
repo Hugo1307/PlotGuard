279e14309940a701cc0a2af9c7035ccce6a378da
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2022-08-14 16:03:16 +0100

    Initial Commit

---
+++ src/main/java/me/hgsoft/minecraft/plotguard/plotguard/PlotGuard.java
@@ -0,0 +1,17 @@
+package me.hgsoft.minecraft.plotguard.plotguard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlotGuard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
